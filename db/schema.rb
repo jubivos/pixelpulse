8a08f9911a58a6c7efcee21675c588265ef048f7
jubivos <uliavostresova9@gmail.com>
2026-06-01 21:26:42 +0300

    Add auth token column to users and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_27_214658) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_29_090057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
@@ -146,15 +146,19 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_27_214658) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.citext "email", null: false
+    t.string "encrypted_password", default: "", null: false
     t.datetime "last_activity_at"
     t.string "login", limit: 50, null: false
     t.string "nickname", limit: 50
-    t.string "password_digest", null: false
     t.datetime "registered_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
     t.bigint "role_id", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["login"], name: "index_users_on_login", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
   end
 
